6e4f1a15a4afe6331f5ab5c12d4e9f9056f38a63
Ahmed <ahmed@spotify.com>
2018-04-30 16:32:00 -0400

    Add test to make sure Functions are invoked through flatMap

--- mobius-rx/src/test/java/com/spotify/mobius/rx/TransformersTest.java
+++ mobius-rx/src/test/java/com/spotify/mobius/rx/TransformersTest.java
@@ -19,15 +19,21 @@
  */
 package com.spotify.mobius.rx;
 
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import com.spotify.mobius.functions.Function;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.awaitility.Duration;
 import org.junit.Before;
 import org.junit.Test;
 import rx.observers.AssertableSubscriber;
+import rx.schedulers.Schedulers;
 import rx.schedulers.TestScheduler;
 import rx.subjects.PublishSubject;
 
@@ -129,5 +135,36 @@ public class TransformersTest {
     upstream.onNext("Hello");
     scheduler.triggerActions();
     observer.assertError(RuntimeException.class);
+  }
+
+  @Test
+  public void processingLongEffectsDoesNotBlockProcessingShorterEffects() {
+    PublishSubject<String> upstream = PublishSubject.create();
+    Function<String, Integer> function =
+        s -> {
+          int length = s.length();
+          try {
+            Thread.sleep(length * 1000);
+          } catch (InterruptedException ie) {
+          }
+          return length;
+        };
+
+    final List<Integer> results = new ArrayList<>();
+    upstream.compose(Transformers.fromFunction(function, Schedulers.io())).subscribe(results::add);
+
+    String f1 = "Hello";
+    String f2 = "Rx1";
+
+    upstream.onNext(f1); // Will take 5 seconds to process
+    upstream.onNext(f2); // Will take 3 seconds to process
+
+    // Since effects are processed in parallel thanks to FlatMap
+    // Therefore we only wait the max time and add a second to
+    // avoid test flakiness thanks to time
+    int delay = Math.max(f1.length(), f2.length()) + 1;
+    await()
+        .atMost(new Duration(delay, TimeUnit.SECONDS))
+        .until(() -> results.equals(Arrays.asList(3, 5)));
   }
 }
--- mobius-rx2/src/test/java/com/spotify/mobius/rx2/TransformersTest.java
+++ mobius-rx2/src/test/java/com/spotify/mobius/rx2/TransformersTest.java
@@ -19,14 +19,21 @@
  */
 package com.spotify.mobius.rx2;
 
+import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 import io.reactivex.functions.Function;
 import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.Schedulers;
 import io.reactivex.schedulers.TestScheduler;
 import io.reactivex.subjects.PublishSubject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.awaitility.Duration;
 import org.junit.Test;
 
 public class TransformersTest {
@@ -110,5 +117,38 @@ public class TransformersTest {
     upstream.onNext("Hello");
     scheduler.triggerActions();
     observer.assertError(RuntimeException.class);
+  }
+
+  @Test
+  public void processingLongEffectsDoesNotBlockProcessingShorterEffects() {
+    PublishSubject<String> upstream = PublishSubject.create();
+    Function<String, Integer> sleepyFunction =
+        s -> {
+          int length = s.length();
+          try {
+            Thread.sleep(length * 1000);
+          } catch (InterruptedException ie) {
+          }
+          return length;
+        };
+
+    final List<Integer> results = new ArrayList<>();
+    upstream
+        .compose(Transformers.fromFunction(sleepyFunction, Schedulers.io()))
+        .subscribe(results::add);
+
+    String f1 = "Hello";
+    String f2 = "Rx2";
+
+    upstream.onNext(f1); // Will take 5 seconds to process
+    upstream.onNext(f2); // Will take 3 seconds to process
+
+    // Since effects are processed in parallel thanks to FlatMap
+    // Therefore we only wait the max time and add a second to
+    // avoid test flakiness thanks to time
+    int delay = Math.max(f1.length(), f2.length()) + 1;
+    await()
+        .atMost(new Duration(delay, TimeUnit.SECONDS))
+        .until(() -> results.equals(Arrays.asList(3, 5)));
   }
 }
