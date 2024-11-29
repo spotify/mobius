5e6b84b4b250b021af7e1d0c07527871dc97f0e2
Rafal Gurzkowski <rafalg@spotify.com>
2024-11-29 15:27:54 +0100

    Add javadoc to MobiusPlugins

--- mobius-core/src/main/java/com/spotify/mobius/MobiusPlugins.java
+++ mobius-core/src/main/java/com/spotify/mobius/MobiusPlugins.java
@@ -13,10 +13,22 @@ public class MobiusPlugins {
     @Nullable
     private static WorkRunner EVENT_RUNNER_OVERRIDE;
 
+    /**
+     * Sets the effect runner that will be used in {@link MobiusLoop} when effectRunner was not
+     * provided to {@link MobiusLoop.Builder}.
+     *
+     * @param defaultEffectRunner the {@link WorkRunner} to use as the default effect runner
+     */
     public static void setDefaultEffectRunner(WorkRunner defaultEffectRunner) {
         EFFECT_RUNNER_OVERRIDE = defaultEffectRunner;
     }
 
+    /**
+     * Sets the event runner that will be used in {@link MobiusLoop} when eventRunner was not
+     * provided to {@link MobiusLoop.Builder}.
+     *
+     * @param defaultEventRunner the {@link WorkRunner} to use as the default event runner
+     */
     public static void setDefaultEventRunner(WorkRunner defaultEventRunner) {
         EVENT_RUNNER_OVERRIDE = defaultEventRunner;
     }
