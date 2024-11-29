ce829f6f892c5439f367abe2cef30fda2a25485b
Rafal Gurzkowski <rafalg@spotify.com>
2024-11-29 11:20:04 +0100

    Add option to override the the default runners

--- mobius-core/src/main/java/com/spotify/mobius/runners/DefaultWorkRunners.java
+++ mobius-core/src/main/java/com/spotify/mobius/runners/DefaultWorkRunners.java
@@ -1,12 +1,36 @@
 package com.spotify.mobius.runners;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public class DefaultWorkRunners {
 
+    @Nullable
+    private static WorkRunner EFFECT_RUNNER_OVERRIDE;
+    @Nullable
+    private static WorkRunner EVENT_RUNNER_OVERRIDE;
+
+    public static void setDefaultEffectRunner(WorkRunner defaultEffectRunner) {
+        EFFECT_RUNNER_OVERRIDE = defaultEffectRunner;
+    }
+
+    public static void setDefaultEventRunner(WorkRunner defaultEventRunner) {
+        EVENT_RUNNER_OVERRIDE = defaultEventRunner;
+    }
+
+    @Nonnull
     public static WorkRunner defaultEffectRunner() {
+        if (EFFECT_RUNNER_OVERRIDE != null) {
+            return EFFECT_RUNNER_OVERRIDE;
+        }
         return WorkRunners.cachedThreadPool();
     }
 
+    @Nonnull
     public static WorkRunner defaultEventRunner() {
+        if (EVENT_RUNNER_OVERRIDE != null) {
+            return EVENT_RUNNER_OVERRIDE;
+        }
         return WorkRunners.singleThread();
     }
 }
